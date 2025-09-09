e6ab4389126a3829ff0c0d3a030275d908e20327
abbasi79 <65632710+abbasi79@users.noreply.github.com>
2025-09-09 08:47:19 -0500

    Update reeadme.py

--- US_Supreme_Court_Predictions/reeadme.py
+++ US_Supreme_Court_Predictions/reeadme.py
@@ -8,4 +8,9 @@ Oyez is the data of plaintiff and respondent winning only
 
 Us class action data  is all about plaintiff, win and loose
 
+
 Since oyez is trained on plaintiff win only, we sort only the winning data from new data
+
+
+To load and process Oyez data use oyez_data_load.py
+To train oyez data use main_models_training.py
